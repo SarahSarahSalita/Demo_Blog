41d7023863ccb2f875dc31b1dd1432eab622d838
Sarah <qwt819@gmail.com>
2017-05-16 17:26:14 +0800

    finish step 4.7

--- index.js
+++ index.js
@@ -27,8 +27,14 @@ app.use(session({
     url: 'mongodb://localhost:27017/myblog'// mongodb 地址
   })
 }));
-// flash 中间价，用来显示通知
+// flash 中间件，用来显示通知
 app.use(flash());
+
+// 处理表单及文件上传的中间件
+app.use(require('express-formidable')({
+	uploadDir: path.join(__dirname,'public/img'),// 上传文件目录
+	keepExtensions: true //保留后缀
+}));
 
 // 设置模板全局常量
 app.locals.blog = {
