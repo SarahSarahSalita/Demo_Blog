6c7059255c3aa657e76127e0b1442ceb21b47793
Sarah <qwt819@gmail.com>
2017-05-17 16:53:11 +0800

    almost done

---
+++ config/production.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongodb: 'mongodb://sarahsarahsalita:bleach631009@@ds141401.mlab.com:41401/mgdb_sarah'
+};
--- routes/signin.js
+++ routes/signin.js
@@ -1,16 +1,34 @@
+var sha1 = require('sha1');
 var express = require('express');
 var router = express.Router();
 
+var UserModel = require('../models/users');
 var checkNotLogin = require('../middlewares/check').checkNotLogin;
 
 // GET /signin ???
 router.get('/', checkNotLogin, function(req, res, next) {
-  res.send(req.flash());
+	res.render('signin');
 });
 
 // POST /signin ????
 router.post('/', checkNotLogin, function(req, res, next) {
-  res.send(req.flash());
+	var name = req.fields.name;
+	var password = req.fields.password;
+	
+	UserModel.getUserByName(name)
+		.then(function (user){
+			if(!user){
+				req.flash('error', '用户不存在');
+				return res.redirect('back');
+			}
+			 req.flash('success', '登录成功');
+			// 用户信息写入 session
+			delete user.password;
+			req.session.user = user;
+			// 跳转到主页
+			res.redirect('/posts');
+		})
+		.catch(next);
 });
 
 module.exports = router;
